8fe7cabe6750dd84f191892bdc133be0544bd5e2
Ariel <84341605+arielsehr@users.noreply.github.com>
2023-04-20 19:38:10 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import { useState, useEffect } from "react"
+
+const App = () => {
+  const [value, setValue] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [previousChats, setPreviousChats] = useState([])
+  const [currentTitle, setCurrentTitle] = useState(null)
+
+  const createNewChat = () => {
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle)
+    setMessage(null)
+    setValue("")
+  }
+
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body:  JSON.stringify({
+        message: value
+      }),
+      headers: {
+        "Content-Type" : "application/json"
+      }
+    }
+    try {
+        const response = await fetch('http://localhost:8000/completions', options)
+        const data = await response.json()
+        setMessage(data.choices[0].message)
+    } catch(error) {
+        console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value)
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats(prevChats => (
+        [...prevChats, 
+          {
+            title: currentTitle,
+            role: "user",
+            content: value
+        }, {
+            title: currentTitle,
+            role: message.role,
+            content: message.content
+        }
+      
+      ]
+      ))
+    }
+  }, [message, currentTitle])
+
+
+  console.log(previousChats)
+
+  const currentChat = previousChats.filter(previousChats => previousChats.title === currentTitle)
+  const uniqueTitles = Array.from(new Set (previousChats.map(previousChat => previousChat.title)))
+  console.log(uniqueTitles)
+
+  return (
+    <div className="app">
+      
+      <section className="side-bar">
+        <button onClick={createNewChat}> + New Chat</button>
+        <ul className="history">
+          {uniqueTitles?.map((uniqueTitle, index) => <li onClick={() => handleClick(uniqueTitle)} key={index}>{uniqueTitle}</li>)}
+        </ul>
+        <nav>
+          <p>Made by Ariel</p>
+        </nav>
+      </section>
+
+      <section className="main">
+        {!currentTitle && <h1>ArielGPT</h1>}
+        <ul className="feed">
+          {currentChat.map((chatMessage, index) => <li key={index}> 
+            <p className="role">{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+          </li>)}
+        </ul>
+
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>Chat →</div>
+          </div>
+
+          <p className="info">
+            This is a clone of Chat GPT Mar 14 Version.
+          </p>
+        </div>
+      </section>
+
+    </div>
+  );
+}
+
+export default App;
